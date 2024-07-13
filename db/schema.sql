b3bb0a53f00263831c53ed68aa202b99b8d41369
Daniel Torres <torres.daniel.0827@gmail.com>
2024-07-13 17:08:21 -0500

    added another table to schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
-\c employees;
+\c employees_db;
 
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
@@ -10,9 +10,18 @@ CREATE TABLE department (
 
 CREATE TABLE role (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(30) NOT NULL
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department INTEGER,
-    FOREIGN KEY (department) REFERENCES department(id) ON DELETE SET NULL
+    department_id INTEGER,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
