cf9581e705077debc23de760db4397c108b075f6
Daniel Torres <torres.daniel.0827@gmail.com>
2024-07-13 11:36:41 -0500

    removed unnecessary code from server.js and added first prompt. Also added file schema.sql in db folder

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-const express = require('express');
-const { Pool } = require('pg');
+const express = require("express");
+const inquirer = require("inquirer");
+const { Pool } = require("pg");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const pool = new Pool (
-    [
-        user = '',
-        password = '',
-        network = 'localhost',
-        database = '',
-    ]    
-);
+const pool = new Pool([
+  (user = ""),
+  (password = ""),
+  (network = "localhost"),
+  (database = ""),
+]);
+
+inquirer.prompt([
+  {
+    type: "list",
+    message: "What would you like to do?",
+    choices: [],
+    name: "action",
+  },
+]);
 
 pool.connect();
 
-app.use(express.json());
-app.use(express,urlencoded({extended: true}));
-
-app.listen(PORT, ()=> {
-    console.log('Hey you did it!')
+app.listen(PORT, () => {
+  console.log("Hey you did it!");
 });
